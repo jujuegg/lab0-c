6bf09fa0299fff1222afa2a3daa2369912612d49
hangps_lab_PC <891028kevin@gmail.com>
2024-02-28 12:41:46 +0800

    Add __glibc_unlikely in line 84 and 104

--- queue.c
+++ queue.c
@@ -81,7 +81,7 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    if (!head || list_empty(head))
+    if (__glibc_unlikely(!head || list_empty(head)))
         return NULL;
 
     struct list_head *first_ptr = head->next;
@@ -101,7 +101,7 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    if (!head || list_empty(head))
+    if (__glibc_unlikely(!head || list_empty(head)))
         return NULL;
 
     struct list_head *last_ptr = head->prev;
